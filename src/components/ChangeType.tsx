aa91b85129dc35b5350ced0fe362cfe8bd9c439f
Ricky Kiamilev <rickyk@udel.edu>
2024-03-05 13:55:14 -0500

    50% done

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,28 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    const tempType: QuestionType =
+                        questionType === "multiple_choice_question"
+                            ? "short_answer_question"
+                            : "multiple_choice_question";
+                    setQuestionType(tempType);
+                }}
+            >
+                Change Type
+            </Button>
+            <p>
+                {questionType === "short_answer_question"
+                    ? "Short Answer"
+                    : "Multiple Choice"}
+            </p>
+        </div>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,44 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState(4);
+    const [isQuizInProgress, setQuizInProgress] = useState(false);
+
+    function startQuiz(): void {
+        setQuizInProgress(true);
+        setAttempts(attempts - 1);
+    }
+
+    function StopQuiz(): void {
+        setQuizInProgress(false);
+    }
+
+    function Mulligan(): void {
+        setAttempts(attempts + 1);
+    }
+    return (
+        <>
+            <div>
+                <div>
+                    Number of Attempts: <span> {attempts} </span>
+                </div>
+                <Button onClick={startQuiz} disabled={isQuizInProgress}>
+                    Start Quiz
+                </Button>
+                <Button onClick={StopQuiz} disabled={!isQuizInProgress}>
+                    Stop Quiz
+                </Button>
+                <Button onClick={Mulligan} disabled={isQuizInProgress}>
+                    Mulligan
+                </Button>
+            </div>
+            <div>
+                {attempts === 0 && isQuizInProgress ? (
+                    <span>Cannot Start Quiz! Out of Attempts!</span>
+                ) : (
+                    <span>All Okay!</span>
+                )}
+            </div>
+        </>
+    );
 }
